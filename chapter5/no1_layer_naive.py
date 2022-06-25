c668948591f6ccde6146cfcf8da675d1f03cce65
nekonlu <appyoohoy@gmail.com>
2022-06-25 16:03:25 +0900

    feat: 各レイヤをimportするため、no1_layer_naive.pyに移行

--- chapter5/no1_layer_naive.py
+++ chapter5/no1_layer_naive.py
@@ -1,3 +1,6 @@
+import numpy as np
+from functions import *
+#%%
 class MulLayer:
     def __init__(self):
         self.x = None
@@ -11,11 +14,11 @@ class MulLayer:
         return out
 
     def backward(self, dout):
-        dx = dout * self.y
-        dy = dout * self.x
+        dx = dout * self.x
+        dy = dout * self.y
 
         return dx, dy
-
+#%%
 class AddLayer:
     def __init__(self):
         pass
@@ -29,8 +32,84 @@ class AddLayer:
         dy = dout * 1
         return dx, dy
 #%%
-a = MulLayer()
-a.forward(12, 32)
-a.backward(1)
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)    # bool型
+        out = x.copy()
+        out[self.mask] = 0;
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+
+#%%
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+#%%
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+#%%
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None    # 損失
+        self.y = None       # softmaxの結果
+        self.t = None       # 教師データ (one-hot vector)
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout = 1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
 #%%
 
+#%%
+
+#%%
+
+#%%
+
+#%%
+
+#%%
